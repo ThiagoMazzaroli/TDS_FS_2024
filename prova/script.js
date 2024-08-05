6bc715950ca43646e7a5fbd099fb98a4a2871417
Thiago Mazzaroli <thiagomazzaroli63@gmail.com>
2024-08-05 20:35:42 -0300

    prova

---
+++ prova/script.js
@@ -0,0 +1,17 @@
+
+function mensagemenviar() {
+    const nome = document.getElementById("nome");
+    const email = document.getElementById("email");
+
+    const mensagem = alert(`Senhor ${nome.value}. Acabamos de enviar um e-mail para: ${email.value}.`);
+
+}
+
+
+
+
+
+
+
+
+
